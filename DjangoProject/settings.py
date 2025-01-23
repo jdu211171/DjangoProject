05fe4363e3c96550f264a5c64c70825a08faa4fd
jdu211171 <211171m@jdu.uz>
2025-01-23 17:49:36 +0500

    CI/CD is being integrated

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-_=#930g#*)cn9p^bn-+zd)yuan!99f))odkbkyj@4v(^4@y-m*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
